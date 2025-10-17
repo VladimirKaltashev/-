b0a36521fc0e4a322739bcda313b798a04d5aefe
Vladimir Kaltashev <vkaltashev@gmail.com>
2025-10-17 14:40:55 +0300

    Добавлена функция генерации рандомной задержки для последующей имитации обращения к серверу. Добавлен спиннер для загрузки который можно использовать через class=spinner

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const remainingCount = document.getElementById('remainingCount');
 addTaskBtn.addEventListener('click', addTask);
 taskInput.addEventListener('input', checkInput);
 
+function getRandomDelay() {
+    return Math.floor(Math.random() * (5000 - 2000 + 1)) + 2000;
+}
+
 function checkInput() {
     if (taskInput.value.trim() === '') {
         addTaskBtn.disabled = true;
